c7215bbfde52697a3d33c090d9c561b253919579
Rubia <rj-rosales@hotmail.com>
2022-10-17 22:48:35 -0300

    Elimine el console log

--- compra.js
+++ compra.js
@@ -11,21 +11,16 @@ let enviar = evento => {
     switch (dtoCat) {
 
         case '1':
-            total=0;
             total = cant * 200 * ((100 - 80) / 100);
-            console.log(total);
-            aPagar.innerHTML+= ` ${total}`;
-           
+            aPagar.innerHTML += ` ${total}`;
             break;
         case '2':
             total = cant * 200 * ((100 - 50) / 100);
-            console.log(total);
-            aPagar.innerHTML+= ` ${total}`;
+            aPagar.innerHTML += ` ${total}`;
             break;
         case '3':
             total = cant * 200 * ((100 - 15) / 100);
-            console.log(total);
-            aPagar.innerHTML+= ` ${total}`;
+            aPagar.innerHTML += ` ${total}`;
             break;
     }
 
