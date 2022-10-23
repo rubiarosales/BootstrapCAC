3b6b571e956af663e2a64e8448bad6f33f7ee060
Rubia <rj-rosales@hotmail.com>
2022-10-23 13:08:25 -0300

    Agregué una validacion en el form cantidad para que verifique si es un numero el dato ingresado y una variale precio

--- compra.js
+++ compra.js
@@ -3,26 +3,47 @@ let btnResumen = document.getElementById("resumen");
 
 let enviar = evento => {
     evento.preventDefault();
+    let precio = 200;
     let aPagar = document.getElementById("saldo");
     var dtoCat = document.getElementById("categoriaInput").value;
     var cant = parseInt(document.getElementById("cantidadInput").value);
     var total;
 
-    switch (dtoCat) {
+    if (isNaN(cant)){
+        aPagar.innerHTML ="Ingrese una cantidad válida";
+    }else{
+            switch (dtoCat) {
 
-        case '1':
-            total = cant * 200 * ((100 - 80) / 100);
-            aPagar.innerHTML += ` ${total}`;
-            break;
-        case '2':
-            total = cant * 200 * ((100 - 50) / 100);
-            aPagar.innerHTML += ` ${total}`;
-            break;
-        case '3':
-            total = cant * 200 * ((100 - 15) / 100);
-            aPagar.innerHTML += ` ${total}`;
-            break;
-    }
+                case '1':
+                    total = cant * precio * ((100 - 80) / 100);
+                    aPagar.innerHTML = `Total a Pagar: $${total}`;
+                    break;
+                case '2':
+                    total = cant * precio * ((100 - 50) / 100);
+                    aPagar.innerHTML = `Total a Pagar: $${total}`;
+                    break;
+                case '3':
+                    total = cant * precio * ((100 - 15) / 100);
+                    aPagar.innerHTML = `Total a Pagar: $${total}`;
+                    break;
+            }
+        }
+    
+    // switch (dtoCat) {
+
+    //     case '1':
+    //         total = cant * precio * ((100 - 80) / 100);
+    //         aPagar.innerHTML += ` ${total}`;
+    //         break;
+    //     case '2':
+    //         total = cant * precio * ((100 - 50) / 100);
+    //         aPagar.innerHTML += ` ${total}`;
+    //         break;
+    //     case '3':
+    //         total = cant * precio * ((100 - 15) / 100);
+    //         aPagar.innerHTML += ` ${total}`;
+    //         break;
+    // }
 
 }
 
